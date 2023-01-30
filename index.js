6d144fb7b92e9c447cb9ee154027d212dfe573d6
Esther Mumo <101511172+mumo-esther@users.noreply.github.com>
2023-01-30 15:10:02 +0300

    added js file for the toggle menu

---
+++ index.js
@@ -0,0 +1,25 @@
+const menu = document.getElementById('menu');
+const open = document.getElementById('open-menu');
+const shut = document.getElementById('close-menu');
+const links = document.querySelectorAll('.navlink');
+shut.style.display = 'none';
+
+open.addEventListener('click', () => {
+  menu.style.left = '0%';
+  shut.style.display = 'block';
+  open.style.display = 'none';
+});
+
+shut.addEventListener('click', () => {
+  menu.style.left = '-90%';
+  shut.style.display = 'none';
+  open.style.display = 'block';
+});
+
+links.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    menu.style.left = '-90%';
+    shut.style.display = 'none';
+    open.style.display = 'block';
+  });
+});
