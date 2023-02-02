9537316b63d470dcfeae92653b2b3af70e1a2d9d
alexiscyber14 <142sledgehammer14@gmail.com>
2023-02-02 10:13:00 +0100

    Fix linter errors

--- index.js
+++ index.js
@@ -601,15 +601,14 @@ const forms = document.querySelector('form');
 const inputs = form.querySelectorAll('input');
 const data = JSON.parse(localStorage.getItem('formData')) || {};
 
-
-inputs.forEach(input => {
+inputs.forEach((input) => {
   const inputName = input.getAttribute('name');
   if (data[inputName]) {
     input.value = data[inputName];
   }
 });
 
-forms.addEventListener('input', e => {
+forms.addEventListener('input', (e) => {
   const input = e.target;
   const inputName = input.getAttribute('name');
   data[inputName] = input.value;
