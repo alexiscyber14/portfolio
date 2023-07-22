c9fef7a527fff4e1b2697ed9ca258bc378d6ff52
alexiscyber14 <142sledgehammer14@gmail.com>
2023-07-23 00:47:45 +0100

    add form

--- index.js
+++ index.js
@@ -17,4 +17,34 @@ const headerMenu =()=>{
       menuBar.style.display='block';
   })
   }
-  headerMenu();
+headerMenu();
+
+
+const quote =()=>{
+    document.getElementById('quoteForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const serviceSelect = document.getElementById('serviceSelect');
+        const selectedService = serviceSelect.value;
+        const loading = document.getElementById('loading');
+        loading.style.display = 'block';
+        setTimeout(() => {
+          const priceRanges = {
+            service0:'select a service',
+            service1: '$10,000 - $300,000',
+            service2: '$500 - $100,000',
+            service3: '$500 - $50,000',
+            service4: '$1000 - $10,000',
+            service5: '$100 - $200',
+            service6: '$900 - $20,000',
+            service7: '$4000 - $20,000',
+            service8: '$500 - $5000',
+            service9: '$0.00 - $200',
+            service10: '$200 - $5000',
+          };
+          const quoteResult = document.getElementById('quoteResult');
+          quoteResult.textContent = `${priceRanges[selectedService]}`;
+          loading.style.display = 'none';
+        }, 1000);
+      });
+  }
+quote();
